2e365dbfab2f88e221835689328601445716d151
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-08-04 00:52:40 +0300

    create_node

--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,7 @@
 #define BINARY_TREE_H
 
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -13,25 +14,26 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
 
-// Binary Search Tree
+/* Binary Search Tree */
 typedef struct binary_tree_s bst_t;
 
-// AVL Tree
+/* AVL Tree */
 typedef struct binary_tree_s avl_t;
 
-// Max Binary Heap
+/* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
 
 
-// Function Prototypes
+/* Function Prototypes */
 void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif
